2aff2c3f6e78393028109de4d4ae8120beb1a290
Guilherme Felipe Ferreira Marcial <guilherme.marcial.felipe@gmail.com>
2016-05-09 12:07:41 -0400

    - Continuação da adição dos cabeçalhos

--- src/map/pc.c
+++ src/map/pc.c
@@ -1,7 +1,38 @@
-// Copyright (c) Hercules Dev Team, licensed under GNU GPL.
-// See the LICENSE file
-// Portions Copyright (c) Athena Dev Teams
-// Tradução feita pela Equipe Cronus-Dev
+//====================================================================\\
+//                   _____                                            ||
+//                  /  __ \                                           ||
+//                  | /  \/_ __ ___  _ __  _   _ ___                  ||
+//                  | |   | '__/ _ \| '_ \| | | / __|                 ||
+//                  | \__/\ | | (_) | | | | |_| \__ \                 ||
+//                   \____/_|  \___/|_| |_|\__,_|___/                 ||
+//                        Source - 2016                               ||
+//====================================================================||
+// = Arquivo:                                                         ||
+// - pc.c                                                             ||
+//====================================================================||
+// = Código Base:                                                     ||
+// - eAthena/Hercules/Cronus                                          ||
+//====================================================================||
+// = Sobre:                                                           ||
+// Este software é livre: você pode redistribuí-lo e/ou modificá-lo   ||
+// sob os termos da GNU General Public License conforme publicada     ||
+// pela Free Software Foundation, tanto a versão 3 da licença, ou     ||
+// (a seu critério) qualquer versão posterior.                        ||
+//                                                                    ||
+// Este programa é distribuído na esperança de que possa ser útil,    ||
+// mas SEM QUALQUER GARANTIA; mesmo sem a garantia implícita de       ||
+// COMERCIALIZAÇÃO ou ADEQUAÇÃO A UM DETERMINADO FIM. Veja a          ||
+// GNU General Public License para mais detalhes.                     ||
+//                                                                    ||
+// Você deve ter recebido uma cópia da Licença Pública Geral GNU      ||
+// juntamente com este programa. Se não, veja:                        ||
+// <http://www.gnu.org/licenses/>.                                    ||
+//====================================================================||
+// = Descrição:                                                       ||
+// Muito importante, contém "o que um jogador pode fazer", como:      ||
+// soltar um item, receber um item, excluir um item,                  ||  
+// contêm dados de bônus tambem.                                      ||
+//====================================================================//
 
 #define HERCULES_CORE
 
