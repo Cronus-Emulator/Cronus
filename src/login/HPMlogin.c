29a6f26c9e1384a1495366a716e5d47ba0bd3cb2
SlexFire <landerfolder@yahoo.com.br>
2016-05-20 02:56:08 -0300

    Inserção de cabeçalho.
    Inserção de cabeçalho nos arquivos-fonte + Termos da Licença GNU GPL.

--- src/login/HPMlogin.c
+++ src/login/HPMlogin.c
@@ -1,5 +1,33 @@
-// Copyright (c) Hercules Dev Team, licensed under GNU GPL.
-// See the LICENSE file
+//====================================================================\\
+//                   _____                                            ||
+//                  /  __ \                                           ||
+//                  | /  \/_ __ ___  _ __  _   _ ___                  ||
+//                  | |   | '__/ _ \| '_ \| | | / __|                 ||
+//                  | \__/\ | | (_) | | | | |_| \__ \                 ||
+//                   \____/_|  \___/|_| |_|\__,_|___/                 ||
+//                        Source - 2016                               ||
+//====================================================================||
+// = Arquivo:                                                         ||
+// - HPMlogin.c                                                       ||
+//====================================================================||
+// = Código Base:                                                     ||
+// - eAthena/Hercules/Cronus                                          ||
+//====================================================================||
+// = Sobre:                                                           ||
+// Este software é livre: você pode redistribuí-lo e/ou modificá-lo   ||
+// sob os termos da GNU General Public License conforme publicada     ||
+// pela Free Software Foundation, tanto a versão 3 da licença, ou     ||
+// (a seu critério) qualquer versão posterior.                        ||
+//                                                                    ||
+// Este programa é distribuído na esperança de que possa ser útil,    ||
+// mas SEM QUALQUER GARANTIA; mesmo sem a garantia implícita de       ||
+// COMERCIALIZAÇÃO ou ADEQUAÇÃO A UM DETERMINADO FIM. Veja a          ||
+// GNU General Public License para mais detalhes.                     ||
+//                                                                    ||
+// Você deve ter recebido uma cópia da Licença Pública Geral GNU      ||
+// juntamente com este programa. Se não, veja:                        ||
+// <http://www.gnu.org/licenses/>.                                    ||
+//====================================================================//
 
 #define HERCULES_CORE
 
