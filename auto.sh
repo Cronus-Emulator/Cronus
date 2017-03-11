1e55dc1f47c5ca8b658dcd97aecadbbd19b537a2
Roberto Oliveira <roberto.oliveira@stoppay.net>
2017-03-11 15:39:38 -0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,8 +1,8 @@
-while true;
-	do
+#!/bin/bash
+
+while true; do
 	services=`ps aux | grep -v grep | grep -cE "map-server|login-server|char-server"`
 	if [ $services -lt 3 ]; then
-		sh cronus desligar
-		sh cronus ligar
+		sh cronus reiniciar
 	sleep 120;
-	done
+done
