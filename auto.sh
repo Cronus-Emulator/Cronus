4925c7e6008aedc526fc84558ab73210c1f37b5a
Roberto Oliveira <roberto.oliveira@stoppay.net>
2017-03-11 15:38:33 -0300

    auto.sh simplificado

--- auto.sh
+++ auto.sh
@@ -1,20 +1,8 @@
 while true;
 	do
-	map=`ps aux | grep -v grep | grep -c map-server`
-	login=`ps aux | grep -v grep | grep -c login-server`
-	char=`ps aux | grep -v grep | grep -c char-server`
-	if [ $map -lt 1 ]
-	then
+	services=`ps aux | grep -v grep | grep -cE "map-server|login-server|char-server"`
+	if [ $services -lt 3 ]; then
 		sh cronus desligar
 		sh cronus ligar
-	elif [ $char -lt 1 ]
-	then
-		sh cronus desligar
-		sh cronus ligar
-	elif [ $login -lt 1 ]
-	then
-		sh cronus desligar
-		sh cronus ligar
-	fi
 	sleep 120;
 	done
